af386f669b5be556404a43834b0b7547c2f16c4d
mr: hetero top <149608626+maylon100@users.noreply.github.com>
2024-07-24 17:04:14 -0300

    f

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// Não é necessário JavaScript para posicionar o elemento no canto da tela, 
-// mas você pode usar o main.js para adicionar comportamentos adicionais ao elemento.
+let nomeUsuário = "";
+let elemento = document.querySelector("#nome-usuário");
 
-// Exemplo de código JavaScript que pode ser usado para interagir com o elemento humanoide:
-var humanoide = document.getElementById('humanoide');
+while(nomeUsuário == ""){
+    nomeUsuário = prompt("Digite seu nome por favor!");
+}
 
-// Exemplo de evento de clique para fazer algo com o humanoide
-humanoide.addEventListener('click', function() {
-  alert('Clicou no humanoide!');
-});
-
-// Outros exemplos de interações podem incluir animações, movimentos, etc.
+if(nomeUsuário == null){
+    elemento.textContent = 'Olá, ';
+}else{
+    elemento.textContent = nomeUsuário;
+}
